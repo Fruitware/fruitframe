703960193a17d9ee7bd7dc1caec74381d1f60efb
bumbu <bmbalex@gmail.com>
2013-07-18 13:17:40 +0300

    add grunt-contrib-template

---
+++ Gruntfile.js
@@ -0,0 +1,136 @@
+'use strict';
+
+module.exports = function(grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      stylus: {
+        files: ['src/style/{,*/}*.styl'],
+        tasks: ['stylus:development'],
+        options: {
+          nospawn: true,
+        }
+      },
+      coffee: {
+        files: ['src/script/{,*/}*.coffee'],
+        tasks: ['coffee:development'],
+        options: {
+          livereload: true
+        }
+      },
+      development: {
+        options: {
+          nospawn: true,
+          livereload: true
+        },
+        files: [
+          'mockup/{,*/}*.html',
+          'mockup/{,*/}*.css',
+          'mockup/{,*/}*.js',
+          'mockup/{,*/}*.{png,jpg,jpeg,gif}'
+        ],
+        tasks: []
+      }
+    },
+    stylus: {
+      development: {
+        options: {
+          urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+          compress: false,
+          linenos: true
+        },
+        files: {
+          'mockup/css/application.css': 'src/style/*.build.styl' // compile and concat into single file
+        }
+      },
+      production: {
+        options: {
+          urlfunc: 'embedurl'
+        },
+        files: {
+          'mockup/css/application.css': 'src/style/*.build.styl'
+        }
+      }
+    },
+    coffee: {
+      development: {
+        options: {
+          sourceMap: true,
+          bare: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/script',
+          src: '*.coffee',
+          dest: 'mockup/js',
+          ext: '.js'
+        }]
+      },
+      production: {
+        options: {
+          bare: true,
+          join: true
+        },
+        files: {
+          'mockup/js/application.js': 'src/script/*.coffee' // concat then compile into single file
+        }
+      }
+    },
+    template: {
+      development: {
+        engine: 'handlebars',
+        cwd: 'src/template/',
+        partials: ['partials/*.hbs'],
+        data: 'src/template/data/data.json',
+        options: {
+        },
+        files: [
+          {
+            expand: true,     // Enable dynamic expansion.
+            cwd: 'src/template/',      // Src matches are relative to this path.
+            src: '*.hbs', // Actual pattern(s) to match.
+            dest: 'app/',   // Destination path prefix.
+            ext: '.html'  // Dest filepaths will have this extension.
+          }
+        ]
+      }
+    },
+    clean: {
+      production: {
+        src: ['mockup/js/*.map']
+      }
+    },
+    copy: {
+      production: {
+        files: [
+          {expand: false, src: 'mockup/css/application.css', dest: 'app/wp-content/themes/flpwindow/css/application.css'},
+          {expand: false, src: 'mockup/js/application.js', dest: 'app/wp-content/themes/flpwindow/js/application.js'},
+          {expand: true, cwd: 'mockup/img/', src: '**', dest: 'app/wp-content/themes/flpwindow/img/', filter: 'isFile'}
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('build', [
+    'stylus:production',
+    'coffee:production',
+    'template',
+    'clean:production',
+    'copy:production'
+  ]);
+
+  grunt.registerTask('server', [
+    'stylus:development',
+    'coffee:development',
+    'watch'
+  ]);
+
+  grunt.registerTask('default', [
+    'server'
+  ]);
+
+};
---
+++ README.md
@@ -0,0 +1,30 @@
+# FruitFrame
+
+## Files and Folders structure
+
+* Grunt.js
+* package.json
+* README.md
+
+* _src_ - Front-end source
+* _mockup_ - Front-end final result
+* _app_ - Application
+* _test_ - Tests
+* _db_ - Database migrations
+
+## Developing front-end
+
+1. install node.js
+*  cd to project folder
+*  run ```npm instal```
+*  run ```grunt```
+
+Now you are in live development mode. Making any changes will compile automatically JS and CSS source.
+
+Before commiting changes run ```grunt build```. This command will compile, minify and copy all the files in the right folders.
+
+## TODO
+
+* Templating
+* Development and Build mode for mockups
+* Sprites generation
